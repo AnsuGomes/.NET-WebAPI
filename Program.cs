565af1ba0df02aa57af6d66463775c057fe7c99d
Anderson Gomes <100328879+AnsuGomes@users.noreply.github.com>
2023-01-15 00:19:18 -0300

    Insert and checking the product.

--- Program.cs
+++ Program.cs
@@ -3,12 +3,33 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.MapPost("/saveproduct", (Product produtc) => {
+    ProductReposiory.Add(produtc);
+});
 
-//DEVE-SE COLOCAR NO HEADERS KEY E VALUE NO POSTMAN.
-app.MapGet("/getproduct", (HttpRequest request) => {
-    return request.Headers["product-code"].ToString();
+app.MapGet("/getproduct/{code}", ([FromRoute] string code) =>{
+    var product = ProductReposiory.GetBy(code);
+    return product;
 });
 
 app.Run();
 
+public static class ProductReposiory{
+    public static List<Product> Products { get; set; }
 
+    public static void Add(Product product){
+        if(Products == null)
+            Products = new List<Product>();
+        Products.Add(product);    
+    }
+
+    public static Product GetBy(string code){
+        return Products.FirstOrDefault(p =>p.Code == code); //If not found, show default response.
+    }
+}
+
+public class Product {
+    public string Code { get; set; }
+
+    public string Name { get; set; }
+}
